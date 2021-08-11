3cdf29634b8a79df35cc1a4c0b975018a3d4b555
liyb <1055025755@qq.com>
2021-08-11 22:57:43 +0800

    熟悉tree

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <Tree
-    ref="tree"
-    :data="data1"
-    show-checkbox
-    check-strictly
-    @on-check-change="handleCheckChange"
-  ></Tree>
+  <div>
+    <Tree
+      ref="tree"
+      :data="data1"
+      show-checkbox
+      check-strictly
+      @on-check-change="handleCheckChange"
+    ></Tree>
+    <Button @click="handleClick()">按钮</Button>
+    <pre>
+      {{ data1 }}
+    </pre>
+  </div>
 </template>
 <script>
 export default {
@@ -19,9 +25,8 @@ export default {
           expand: true, // 是否展开
           children: [
             {
-              title: "parent 1-1",
+              title: "leaf 1-1-1",
               expand: true,
-              checked: true,
               children: [
                 {
                   title: "leaf 1-1-1",
@@ -31,32 +36,50 @@ export default {
                 },
               ],
             },
-            {
-              title: "parent 1-2",
-              expand: true,
-              children: [
-                {
-                  title: "leaf 1-2-1",
-                },
-                {
-                  title: "leaf 1-2-1",
-                },
-              ],
-            },
           ],
         },
       ],
     };
   },
+  mounted() {},
   methods: {
     handleCheckChange(data) {
-      this.$nextTick(() => {
-        this.data1[0].disableCheckbox = true;
-        console.log(
-          "🚀 ~ file: Home.vue ~ line 59 ~ this.$nextTick ~ this.data1",
-          this.data1
-        );
+      console.log(
+        "🚀 ~ file: Home.vue ~ line 46 ~ handleCheckChange ~ data",
+        data
+      );
+      const hasLeaf112 = data.find((item) => {
+        return item.title === "leaf 1-1-2";
       });
+
+      if (hasLeaf112) {
+        const leaf111 = this.data1[0].children[0].children[0];
+        this.$set(leaf111, "disableCheckbox", true);
+      }
+
+      // const leaf1 = data.find((item) => item.title === "leaf 1-1-1");
+      // const leaf2 = data.find((item) => item.title === "leaf 1-1-2");
+      // if (leaf1) {
+      //   this.$set(
+      //     this.data1[0].children[0].children[1],
+      //     "disableCheckbox",
+      //     true
+      //   );
+      // }
+
+      // if (leaf2) {
+      //   this.$set(
+      //     this.data1[0].children[0].children[0],
+      //     "disableCheckbox",
+      //     true
+      //   );
+      // }
+    },
+    handleClick() {
+      console.log(
+        "🚀 ~ file: Home.vue ~ line 46 ~ mounted ~ this.$refs.tree",
+        this.$refs.tree.getCheckedNodes()
+      );
     },
   },
 };
